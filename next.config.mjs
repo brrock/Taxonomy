e870ae61cb75bd68da04b96f799c6e0920ee85bc
zce <w@zce.me>
2024-06-12 18:48:20 +0800

    feat: start velite without webpack plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,17 @@
 import "./env.mjs"
-import { build } from "velite"
+
+const isDev = process.argv.indexOf('dev') !== -1
+const isBuild = process.argv.indexOf('build') !== -1
+if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
+  process.env.VELITE_STARTED = '1'
+  const { build } = await import('velite')
+  await build({ watch: isDev, clean: !isDev })
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["avatars.githubusercontent.com"],
-  },
-  webpack: config => {
-    config.plugins.push(new VeliteWebpackPlugin())
-    return config
-  }
-}
-
-class VeliteWebpackPlugin {
-  static started = false
-  apply(compiler) {
-    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
-      if (VeliteWebpackPlugin.started) return
-      VeliteWebpackPlugin.started = true
-      const dev = compiler.options.mode === 'development'
-      await build({ watch: dev, clean: !dev })
-    })
   }
 }
 
